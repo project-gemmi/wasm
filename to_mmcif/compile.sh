a4b3d8c8db4f3f88009160c1e7602bdfc5828212
Marcin Wojdyr <wojdyr@gmail.com>
2020-11-09 14:43:13 +0100

    Update. Now two mtz files (merge+unmerged) can be converted together.

--- to_mmcif/compile.sh
+++ to_mmcif/compile.sh
@@ -5,7 +5,7 @@ em++ -Os -Wall -Wextra -std=c++20 -I../../gemmi/include \
     -s STRICT=1 \
     -s DISABLE_EXCEPTION_CATCHING=0 \
     -s ALLOW_MEMORY_GROWTH=1 \
-    -s 'EXPORTED_FUNCTIONS=["_pdb2cif", "_mtz2cif", "_clear_string", "_get_version"]' \
+    -s 'EXPORTED_FUNCTIONS=["_pdb2cif", "_mtz2cif", "_mtzpair2cif", "_clear_string", "_get_str2", "_get_version"]' \
     -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["ccall", "writeArrayToMemory"]' \
     pdb2cif.cpp -o pdb2cif.js \
     #--emrun
--- to_mmcif/pdb2cif.cpp
+++ to_mmcif/pdb2cif.cpp
@@ -14,6 +14,7 @@
 #include <gemmi/align.hpp>     // for assign_label_seq_id
 
 std::string global_str;
+std::string global_str2;
 
 extern "C" {
 
@@ -60,8 +61,42 @@ const char* mtz2cif(char* data, size_t size) {
   return global_str.c_str();
 }
 
+const char* mtzpair2cif(char* data1, size_t size1,
+                        char* data2, size_t size2) {
+  try {
+    gemmi::Mtz mtz1;
+    mtz1.read_stream(gemmi::MemoryStream(data1, data1 + size1), true);
+    std::free(data1);
+
+    gemmi::Mtz mtz2;
+    mtz2.read_stream(gemmi::MemoryStream(data2, data2 + size2), true);
+    std::free(data2);
+
+    std::ostringstream validate_out;
+    validate_merged_intensities(mtz1, mtz2, validate_out);
+    global_str2 = validate_out.str();
+
+    mtz1.switch_to_original_hkl();
+    mtz2.switch_to_original_hkl();
+
+    std::ostringstream os;
+    gemmi::MtzToCif mtz_to_cif;
+    mtz_to_cif.write_cif(mtz1, &mtz2, os);
+    global_str = os.str();
+  } catch (std::runtime_error& e) {
+    global_str = "ERROR: ";
+    global_str += e.what();
+  }
+  return global_str.c_str();
+}
+
+const char* get_str2() {
+  return global_str2.c_str();
+}
+
 void clear_string() {
   global_str.clear();
+  global_str2.clear();
 }
 
 } // extern "C"
